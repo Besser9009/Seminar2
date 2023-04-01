154556658d762b1dc9f168e38c41868378649b28
besser <besser9009@mail.ru>
2023-04-02 01:58:24 +0300

    add num2

---
+++ num2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+//Первое решение задачи
+int number = new Random(). Next(100, 1000);
+int num1 = number / 100;
+int num3 = number % 10;
+Console.WriteLine(number);
+Console.WriteLine($"{num1}{num3}");
+
+//Второе решение задачи
+int number = new Random(). Next(100, 1000);
+Console.WriteLine(number);
+int num1 = number / 100;
+int num3 = number % 10;
+int IscNum = num1 * 10 + num3;
+System.Console.WriteLine(IscNum);
