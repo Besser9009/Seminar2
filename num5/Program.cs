18c84b4a6b2cdd130136f2c0f048f4eadfb334ed
besser <besser9009@mail.ru>
2023-04-02 03:22:56 +0300

    add num5

---
+++ num5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+System.Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 * num1 == num2){
+    System.Console.WriteLine("Да второе число является квадратом первого");
+}
+else{
+    System.Console.WriteLine("Нет второе число не является квадратом первого");
+}
